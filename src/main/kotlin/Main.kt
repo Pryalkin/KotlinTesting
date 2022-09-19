bb4440d8a9ea96907332c998d05bc8a70cb0b4da
pryalkin <pppryalkin@gmail.com>
2022-09-19 13:18:47 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>){
+    var name: String = "Pavel"
+    var yearOfBirth: Int = 1990
+    var givenYear: Int = 2022
+    println("Hello! My name is $name")
+    println("I am ${givenYear-yearOfBirth} years old")
+}
